64bf95bd24a95b48f7a67cb41143b80499ecbe9e
Madhav Bhardwaj <118822264+madhavwaj@users.noreply.github.com>
2023-04-05 17:17:05 +0530

    Update q10.java

--- Assignment 1/q10.java
+++ Assignment 1/q10.java
@@ -1,9 +1,17 @@
-
-public class q10 {
+public class q_10 {
 
 	public static void main(String[] args) {
-			
-
+		// TODO Auto-generated method stub
+		String ruler="1";
+		System.out.println(ruler);
+		String a= ruler + "2" + ruler;
+		System.out.println(a);
+		String b= ruler + "3" + ruler;
+		System.out.println(b);
+		String c= ruler + 4 + ruler;
+		System.out.println(c);
+	
+		
 	}
 
 }
